047c0567e83407cdb05dd951891fb9fde083b1eb
Bartosz Stepien <bartoszstepien01@interia.pl>
2021-10-15 16:33:58 +0200

    Add video grid

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,24 +4,28 @@
 	let video;
 
 	onMount(async() => {
+		window.dispatchEvent(new Event('resize'));
+
 		const peerjs = await import("peerjs");
 		const Peer = peerjs.default;
 
 		let peer = new Peer();
-		let stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+		let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
 
 		video.srcObject = stream;
-		video.play();
 	});
 </script>
 
 <svelte:head>
 	<title>Home</title>
+	<link rel="stylesheet" href="/css/grid.css">
 	<script>
 		let parcelRequire;
 	</script>
 </svelte:head>
 
-<video bind:this={video}>
-	<track kind="captions">
-</video>
+<div id="gallery">
+	<div class="video-container">
+		<video autoplay muted bind:this={video}></video>
+	</div>
+</div>
