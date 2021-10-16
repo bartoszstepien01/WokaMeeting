9db1ba9a4d66c9754394f41153abb9bf87ff63f3
Bartosz Stepien <bartoszstepien01@interia.pl>
2021-10-16 17:06:23 +0200

    Add deploy action

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import static_adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,12 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: static_adapter(),
+		paths: {
+			base: 'WokaMeeting',
+		},
+		appDir: 'internal'
 	}
 };
 
