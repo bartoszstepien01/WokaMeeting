987eade10591095f49f7da64acdcdef4e34bb858
bartoszstepien01 <bartoszstepien01@interia.pl>
2023-06-08 01:52:04 +0200

    Bugfix 3

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,6 @@
 	import '../app.css';
 	import { pwaInfo } from 'virtual:pwa-info'; 
 
-	export const prerender = true;
-
   	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : ''
 </script>
 
