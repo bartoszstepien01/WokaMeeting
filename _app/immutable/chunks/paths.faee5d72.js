0e2386d7e5e8bb8620fe1ded9bdb390c330526fe
bartoszstepien01 <bartoszstepien01@users.noreply.github.com>
2023-06-20 17:59:01 +0000

    deploy: 1c033477d116027f1265fee1197ca2ef1cf3a283

---
+++ _app/immutable/chunks/paths.faee5d72.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1nytywn)==null?void 0:s.base)??"/WokaMeeting";var t;const a=((t=globalThis.__sveltekit_1nytywn)==null?void 0:t.assets)??e;export{a,e as b};
--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,33 @@
-const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r = [
-  e + "/_app/immutable/entry/app.65dc8e00.js",
-  e + "/_app/immutable/assets/0.96710f24.css",
-  e + "/_app/immutable/nodes/0.93c62b8b.js",
-  e + "/_app/immutable/nodes/1.e434b7e9.js",
-  e + "/_app/immutable/nodes/2.3ea59a02.js",
-  e + "/_app/immutable/nodes/3.bc8342d9.js",
-  e + "/_app/immutable/assets/JoinPopup.c6583623.css",
-  e + "/_app/immutable/chunks/JoinPopup.de206de2.js",
-  e + "/_app/immutable/chunks/index.5227cc00.js",
-  e + "/_app/immutable/chunks/paths.635d698a.js",
-  e + "/_app/immutable/chunks/preload-helper.41c905a7.js",
-  e + "/_app/immutable/chunks/public.c29bcb9f.js",
-  e + "/_app/immutable/chunks/singletons.e08de900.js",
-  e + "/_app/immutable/entry/start.de689f4e.js",
-  e + "/_app/immutable/chunks/Host.0111f10a.js",
-  e + "/_app/immutable/chunks/Peer.2320ed2a.js"
+const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r = [
+  s + "/_app/immutable/entry/app.4abae16b.js",
+  s + "/_app/immutable/assets/0.96710f24.css",
+  s + "/_app/immutable/nodes/0.93c62b8b.js",
+  s + "/_app/immutable/nodes/1.bf049c04.js",
+  s + "/_app/immutable/nodes/2.6b84c87f.js",
+  s + "/_app/immutable/nodes/3.44a1d616.js",
+  s + "/_app/immutable/assets/JoinPopup.c6583623.css",
+  s + "/_app/immutable/chunks/JoinPopup.de206de2.js",
+  s + "/_app/immutable/chunks/index.5227cc00.js",
+  s + "/_app/immutable/chunks/paths.faee5d72.js",
+  s + "/_app/immutable/chunks/preload-helper.41c905a7.js",
+  s + "/_app/immutable/chunks/public.c29bcb9f.js",
+  s + "/_app/immutable/chunks/singletons.7f939d13.js",
+  s + "/_app/immutable/entry/start.a93d6e59.js",
+  s + "/_app/immutable/chunks/Host.0111f10a.js",
+  s + "/_app/immutable/chunks/Peer.2320ed2a.js"
 ], u = [
-  e + "/favicon.png",
-  e + "/manifest.json"
-], o = "1686345185352", c = self, p = `cache${o}`, h = r.concat(u), m = new Set(h);
-c.addEventListener("install", (s) => {
-  s.waitUntil(
+  s + "/favicon.png",
+  s + "/manifest.json"
+], o = "1687283931008", c = self, p = `cache${o}`, h = r.concat(u), m = new Set(h);
+c.addEventListener("install", (e) => {
+  e.waitUntil(
     caches.open(p).then((t) => t.addAll(h)).then(() => {
       c.skipWaiting();
     })
   );
 });
-c.addEventListener("activate", (s) => {
-  s.waitUntil(
+c.addEventListener("activate", (e) => {
+  e.waitUntil(
     caches.keys().then(async (t) => {
       for (const a of t)
         a !== p && await caches.delete(a);
@@ -35,23 +35,23 @@ c.addEventListener("activate", (s) => {
     })
   );
 });
-async function d(s) {
+async function d(e) {
   const t = await caches.open(`offline${o}`);
   try {
-    const a = await fetch(s);
-    return t.put(s, a.clone()), a;
+    const a = await fetch(e);
+    return t.put(e, a.clone()), a;
   } catch (a) {
-    const n = await t.match(s);
+    const n = await t.match(e);
     if (n)
       return n;
     throw a;
   }
 }
-c.addEventListener("fetch", (s) => {
-  if (s.request.method !== "GET" || s.request.headers.has("range"))
+c.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET" || e.request.headers.has("range"))
     return;
-  const t = new URL(s.request.url), a = t.protocol.startsWith("http"), n = t.hostname === self.location.hostname && t.port !== self.location.port, i = t.host === self.location.host && m.has(t.pathname), l = s.request.cache === "only-if-cached" && !i;
-  a && !n && !l && s.respondWith(
-    (async () => i && await caches.match(s.request) || d(s.request))()
+  const t = new URL(e.request.url), a = t.protocol.startsWith("http"), n = t.hostname === self.location.hostname && t.port !== self.location.port, i = t.host === self.location.host && m.has(t.pathname), l = e.request.cache === "only-if-cached" && !i;
+  a && !n && !l && e.respondWith(
+    (async () => i && await caches.match(e.request) || d(e.request))()
   );
 });
